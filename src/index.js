ffbe10b35c7b1f7a26b373e76ceb886c61e5a0a5
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-01-23 17:11:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter basename="/project-team5-frontend">
+        <BrowserRouter basename="/node_front_fs_85_groguls">
           <ThemeProvider theme={theme}>
             <LocalizationProvider
               dateAdapter={AdapterDateFns}
